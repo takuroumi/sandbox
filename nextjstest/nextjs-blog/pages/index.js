084ce232c0cd13946fe07218d28141f79b5e3345
takuroumi <takuro.umihara@outlook.com>
2022-09-19 17:15:09 +0900

    Dynamic Routes done

--- nextjstest/nextjs-blog/pages/index.js
+++ nextjstest/nextjs-blog/pages/index.js
@@ -2,8 +2,18 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,11 +26,21 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <h2>
-        <Link href="/posts/first-post">
-            <a>Go to first-post</a>
-        </Link>
-      </h2>
+
+      <section className={`{$utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   );
 }
+
